32bb5ba19ec3a8cee8573522030037296d85ea90
ersonp <ersonpereiracr7@gmail.com>
2021-03-31 22:57:08 +0530

    3. Add Tasks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react'
 import Web3 from 'web3'
-import './App.css';
+import './App.css'
 import {TODO_LIST_ABI, TODO_LIST_ADDRESS} from './config'
+import TodoList from './TodoList'
 
 class App extends Component{
 
@@ -10,7 +11,36 @@ class App extends Component{
   }
   
   async loadBlockchainData() {
-    const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+    var web3 = new Web3()
+    if (window.ethereum) {
+      web3 = new Web3(window.ethereum);
+      try {
+        window.ethereum.enable().then(function() {
+          // User has allowed account access to DApp...
+        });
+      } catch (e) {
+        // User has denied account access to DApp...
+      }
+    }
+    // Legacy DApp Browsers
+    else if (window.web3) {
+      web3 = new Web3(web3.currentProvider);
+    }
+    // Non-DApp Browsers
+    else {
+      alert("You have to install MetaMask !");
+    }
+    window.ethereum.enable();
+    console.log(" typoe of = ", typeof web3);
+    if (typeof web3 != "undefined") {
+      this.web3Provider = web3.currentProvider;
+      window.ethereum.enable();
+    } else {
+      this.web3Provider = new Web3.providers.HttpProvider(
+        "http://127.0.0.1:7545"
+      );
+      window.ethereum.enable();
+    }
     const network = await web3.eth.net.getNetworkType();
     const accounts = await web3.eth.getAccounts()
     this.setState({ account: accounts[0] })
@@ -24,6 +54,8 @@ class App extends Component{
         tasks: [...this.state.tasks, task]
       })
     }
+    this.setState({ loading: false })
+    console.log(taskCount)
   }
 
   constructor(props) {
@@ -31,8 +63,18 @@ class App extends Component{
     this.state = { 
       account: '',
       taskCount: 0,
-      tasks: []
+      tasks: [],
+      loading: true
     }
+    this.createTask = this.createTask.bind(this)
+  }
+
+  createTask(content) {
+    this.setState({ loading: true })
+    this.state.todoList.methods.createTask(content).send({ from: this.state.account })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
   }
 
   render (){
@@ -49,29 +91,10 @@ class App extends Component{
         <div className="container-fluid">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex justify-content-center">
-              <div id="loader" className="text-center">
-                <p className="text-center">Loading...</p>
-              </div>
-              <div id="content">
-                <form >
-                  <input id="newTask" type="text" className="form-control" placeholder="Add task..." required />
-                  <input type="submit" hidden="" />
-                </form>
-                <ul id="taskList" className="list-unstyled">
-                  { this.state.tasks.map((task, key) => {
-                    return(
-                      <div className="taskTemplate" className="checkbox" key={key}>
-                        <label>
-                          <input type="checkbox" />
-                          <span className="content">{task.content}</span>
-                        </label>
-                      </div>
-                    )
-                  })}
-                </ul>
-                <ul id="completedTaskList" className="list-unstyled">
-                </ul>
-              </div>
+              { this.state.loading
+                ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
+                : <TodoList tasks={this.state.tasks} createTask={this.createTask} />
+              }
             </main>
           </div>
         </div>
